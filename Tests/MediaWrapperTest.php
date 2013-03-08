6c09f26b1abe9e489b4e92d0ca60d9c1d7a3b6d1
Hai-Nam Nguyen <hainam@jcisio.com>
2013-03-08 23:09:35 +0100

    Add more assertions on the thumbnail

--- Tests/MediaWrapperTest.php
+++ Tests/MediaWrapperTest.php
@@ -9,9 +9,17 @@ class MediaWrapperTest extends PHPUnit_Framework_TestCase {
     include_once __DIR__ . '/../MediaWrapper/MediaWrapper.php';
   }
 
+  function assertImageUrl($url) {
+    $size = getimagesize($url);
+    $this->assertTrue(is_array($size));
+    $this->assertArrayHasKey('mime', $size);
+    $this->assertStringStartsWith('image/', $size['mime']);
+  }
+
   public function testYoutube() {
     $m = MediaWrapper::getInstance()->getWrapper('http://www.youtube.com/watch?v=9bZkp7q19f0');
     $this->assertEquals('http://img.youtube.com/vi/9bZkp7q19f0/0.jpg', $m->thumbnail());
+    $this->assertImageUrl($m->thumbnail());
     $this->assertEquals('<iframe class="youtube-player" type="text/html" width="560" height="315" src="http://www.youtube.com/embed/9bZkp7q19f0?wmode=transparent" frameborder="0"></iframe>', $m->player());
 
     $m->player_options(array('width' => '200', 'height' => '100'));
@@ -20,6 +28,7 @@ class MediaWrapperTest extends PHPUnit_Framework_TestCase {
 
   public function testVimeo() {
     $m = MediaWrapper::getInstance()->getWrapper('http://vimeo.com/56488043');
+    $this->assertImageUrl($m->thumbnail());
     $this->assertEquals('http://b.vimeocdn.com/ts/391/133/391133096_640.jpg', $m->thumbnail());
     $this->assertEquals('<iframe class="vimeo-player" type="text/html" width="560" height="315" src="http://player.vimeo.com/video/56488043?title=0&amp;byline=0&amp;portrait=0" frameborder="0"></iframe>', $m->player());
   }
