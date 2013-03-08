6aa541a6ba502f7503b4c59c6ac77d8d285ce420
Hai-Nam Nguyen <hainam@jcisio.com>
2013-03-08 22:09:52 +0100

    Add tests for Vimeo

--- Tests/MediaWrapperTest.php
+++ Tests/MediaWrapperTest.php
@@ -19,5 +19,11 @@ class MediaWrapperTest extends PHPUnit_Framework_TestCase {
     $m->player_options(array('width' => '200', 'height' => '100'));
     $this->assertEquals('<iframe class="youtube-player" type="text/html" width="200" height="100" src="http://www.youtube.com/embed/vyfzw09jjEo?wmode=transparent" frameborder="0"></iframe>', $m->player());
   }
+
+  public function testVimeo() {
+    $m = MediaWrapper::getInstance()->getWrapper('http://vimeo.com/56488043');
+    $this->assertEquals('http://b.vimeocdn.com/ts/391/133/391133096_640.jpg', $m->thumbnail());
+    $this->assertEquals('<iframe class="vimeo-player" type="text/html" width="560" height="315" src="http://player.vimeo.com/video/56488043?title=0&amp;byline=0&amp;portrait=0" frameborder="0"></iframe>', $m->player());
+  }
 }
 
