5ac79a5085daf569ff1df7b54cfbef3abbd17c82
Hai-Nam Nguyen <hainam@jcisio.com>
2013-04-08 22:32:31 +0200

    Add Tests for overriden options

--- Tests/MediaWrapperTest.php
+++ Tests/MediaWrapperTest.php
@@ -24,6 +24,12 @@ class MediaWrapperTest extends PHPUnit_Framework_TestCase {
 
     $m->player_options(array('width' => '200', 'height' => '100'));
     $this->assertEquals('<iframe class="youtube-player" type="text/html" width="200" height="100" src="http://www.youtube.com/embed/9bZkp7q19f0?wmode=transparent" frameborder="0"></iframe>', $m->player());
+
+    // Now pass directly the options
+    $this->assertEquals('<iframe class="youtube-player" type="text/html" width="400" height="200" src="http://www.youtube.com/embed/9bZkp7q19f0?wmode=transparent" frameborder="0"></iframe>', $m->player(array('width' => '400', 'height' => '200')));
+
+    // Test if options are overriden by the last passed option.
+    $this->assertEquals('<iframe class="youtube-player" type="text/html" width="560" height="315" src="http://www.youtube.com/embed/9bZkp7q19f0?wmode=transparent" frameborder="0"></iframe>', $m->player());
   }
 
   public function testVimeo() {
