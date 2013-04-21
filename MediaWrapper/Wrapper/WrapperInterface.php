6a8082dbf31c43ff0af30b393697378298dbcd60
Hai-Nam Nguyen <hainam@jcisio.com>
2013-04-21 14:53:03 +0200

    Update WrapperInterface

--- MediaWrapper/Wrapper/WrapperInterface.php
+++ MediaWrapper/Wrapper/WrapperInterface.php
@@ -20,16 +20,20 @@ interface WrapperInterface {
   public function player(array $options = array());
 
   /**
-   * Sets options for the player
+   * Fill default options.
    *
-   * @param array $options Player options.
+   * @param array $options
    *   Pass NULL to a key to remove an option from default value (e.g.
    *   array('wmode' => NULL) to remove the default 'wmode' setting from
    *   Youtube).
    *
-   * @see player()
+   * @param boolean $set
+   *   Whether to set the $this->options value.
+   *
+   * @return
+   *   Combined options array.
    */
-  public function player_options(array $options);
+  public function player_options(array $options, $set = TRUE);
 
 }
 
